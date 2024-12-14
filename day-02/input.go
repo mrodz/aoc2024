22c5affa77cdf2c4ecf8ab161d61c146aa6092b8
mrodz <79176075+mrodz@users.noreply.github.com>
2024-12-13 17:55:25 -0800

    day 2 (part 2)

--- day-02/input.go
+++ day-02/input.go
@@ -30,6 +30,126 @@ func (n *NuclearReport) safeReports() ([]int, error) {
 	return result, nil
 }
 
+func (n *NuclearReport) safeReportsWithDampener() ([]int, error) {
+	var result []int
+
+	for index, row := range n.grid {
+		safe, err := IsRowSafeWithDampener(row)
+
+		if err != nil {
+			return nil, err
+		} else if !safe {
+			continue
+		}
+
+		result = append(result, index)
+	}
+
+	return result, nil
+}
+
+func IsRowSafeWithDampener(row []uint8) (bool, error) {
+	return isRowSafeWithDampenerRecursive(row, -1)
+}
+
+func processNeighbors(row []uint8, index int) (bool, error) {
+	// Correct for:
+	// A) "Any two adjacent levels differ by at least one and at most three."
+	// B) "The levels are either all increasing or all decreasing." (divergence from established acceleration)
+	if index > 0 {
+		l, err := isRowSafeWithDampenerRecursive(row, index-1)
+		if err != nil {
+			return false, err
+		}
+		if l {
+			return true, nil
+		}
+	}
+
+	// Correct for:
+	// A) "The levels are either all increasing or all decreasing." (possible faulty initial acceleration)
+	if index == 2 {
+		l, err := isRowSafeWithDampenerRecursive(row, 0)
+		if err != nil {
+			return false, err
+		}
+		if l {
+			return true, nil
+		}
+		r, err := isRowSafeWithDampenerRecursive(row, 1)
+		if err != nil {
+			return false, err
+		}
+		if r {
+			return true, nil
+		}
+	}
+
+	m, err := isRowSafeWithDampenerRecursive(row, index)
+	if err != nil {
+		return false, err
+	}
+	return m, nil
+}
+
+func isRowSafeWithDampenerRecursive(row []uint8, skip int) (bool, error) {
+	if len(row) < 2 {
+		return false, errors.New("cannot check the safety of a row of less than two elements")
+	}
+
+	var acceleration int8
+
+	start_index := 0
+
+	if skip == 0 {
+		start_index = 1
+	}
+
+	prev := row[start_index]
+
+	for i := start_index + 1; i < len(row); i++ {
+		if skip == i {
+			continue
+		}
+
+		this := row[i]
+
+		difference := max(this, prev) - min(this, prev)
+
+		if difference < 1 || difference > 3 {
+			if skip == -1 {
+				return processNeighbors(row, i)
+			}
+
+			return false, nil
+		}
+
+		if acceleration == 0 {
+			acceleration = int8(this) - int8(prev)
+		}
+
+		if acceleration > 0 && this < prev {
+			if skip == -1 {
+				return processNeighbors(row, i)
+			}
+
+			return false, nil
+		}
+
+		if acceleration < 0 && this > prev {
+			if skip == -1 {
+				return processNeighbors(row, i)
+			}
+
+			return false, nil
+		}
+
+		prev = this
+	}
+
+	return true, nil
+}
+
 func IsRowSafe(row []uint8) (bool, error) {
 	if len(row) < 2 {
 		return false, errors.New("cannot check the safety of a row of less than two elements")
--- day-02/main.go
+++ day-02/main.go
@@ -14,7 +14,21 @@ func PartOne(report *NuclearReport) error {
 
 	safe := len(safeReports)
 
-	fmt.Printf("Analyze the unusual data from the engineers. How many reports are safe? %d", safe)
+	fmt.Printf("Analyze the unusual data from the engineers. How many reports are safe? The Answer is %d\n", safe)
+
+	return nil
+}
+
+func PartTwo(report *NuclearReport) error {
+	safeReports, err := report.safeReportsWithDampener()
+
+	if err != nil {
+		return err
+	}
+
+	safe := len(safeReports)
+
+	fmt.Printf("Update your analysis by handling situations where the Problem Dampener can remove a single level from unsafe reports. How many reports are now safe? The Answer is %d", safe)
 
 	return nil
 }
@@ -27,4 +41,5 @@ func main() {
 	}
 
 	PartOne(report)
+	PartTwo(report)
 }
